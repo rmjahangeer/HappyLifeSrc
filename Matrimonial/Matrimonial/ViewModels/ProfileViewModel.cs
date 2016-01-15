1302bf8a33f3ad037aa7ad5c4e75e801b6421fdf
Rana M.Jahangir <callrmj@gmail.com>
2016-01-15 21:36:35 +0500

    solution

---
+++ Matrimonial/Matrimonial/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Matrimonial.ClientModels;
+using Matrimonial.Models;
+
+namespace Matrimonial.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public ProfileViewModel()
+        {
+            Heights = new List<Height>();
+            Occupations = new List<Occupation>();
+            UserProfile = new UserProfileModel();
+            Requests = new List<Request>();
+        }
+        public List<Height> Heights { get; set; }
+        public List<Occupation> Occupations { get; set; }
+        public UserProfileModel UserProfile { get; set; }
+        public string DateOfBirthString { get; set; }
+        public List<Request> Requests { get; set; }
+    }
+}
